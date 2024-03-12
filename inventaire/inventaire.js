3e1c95daffb1d6e65a29013e900b4bdea64d9cdb
CatsPlay <161060360+CatsPlay@users.noreply.github.com>
2024-03-12 13:13:37 +0100

    Update inventaire.js

--- inventaire/inventaire.js
+++ inventaire/inventaire.js
@@ -6,3 +6,12 @@ const APIurl = "...";
 fetch(APIurl)
     .then(response => response.json())
     .then(data =>{
+
+        console.log(`liste ${name}:`);
+        data.foeEach{repo => {
+            console.log(`- ${repo.name}`);
+        });
+})
+.catch(error => {
+    console.error(`Erreur lors de l'appel API : ${error.message}`);
+});
